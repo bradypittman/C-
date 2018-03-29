a03a904e18d549d621e8aec5fd9359d8fde281e0
Brady Pittman <34654133+bradypittman@users.noreply.github.com>
2018-03-28 21:34:02 -0400

    Update Smalltalk_American.cpp

--- P5/Smalltalk_American.cpp
+++ P5/Smalltalk_American.cpp
@@ -1,70 +1,17 @@
-/*
- * Smalltalk.cpp
- *
- *  Created on: Nov 8, 2017
- *      Author: Brady
- */
+#pragma once
+#include <vector>
+#include "Smalltalk.h"
 
-#include "./includes/Smalltalk.h"
-#include "./includes/constants.h"
+class Smalltalk_American :
+	public Smalltalk
+{
+public:
+	//create with appropriate phrases
+	//use base class call to set Nationality (See constants for Nationality strings)
+	Smalltalk_American(int i =1);
+	Smalltalk_American(std::string myNationality,int i =1);
+	
+	~Smalltalk_American(void);
+	virtual void populatePhrases();
+};
 
-
-Smalltalk::Smalltalk(std::string myNationality, int iPerson) :
-	nationality(myNationality),
-	iPerson(iPerson),
-	current_phrase(0),
-	pWatch(NULL) {
-
-}
-
-Smalltalk::~Smalltalk(void) {
-
-	if (pWatch != NULL) {
-		delete pWatch;
-		pWatch = NULL;
-	}
-}
-
-std::string Smalltalk::saySomething() {
-	std::string say(nationality + " " + std::to_string(iPerson) + ":");
-
-	if (mySmallTalk.size() > 0) {
-		current_phrase = (current_phrase + 1) % mySmallTalk.size();
-		say += mySmallTalk[current_phrase];
-	}
-
-	return say;
-}
-
-std::string Smalltalk::getTime() {
-
-	std::string str(nationality + " " + std::to_string(iPerson) + ":");
-
-	if (pWatch == NULL) {
-		str += I_DO_NOT_HAVE_A_WATCH;
-	} else {
-		str += pWatch->getTime();
-	}
-
-	return str;
-}
-
-Watch* Smalltalk::takeWatch() {
-
-	Watch* p = this->pWatch;
-	if (p != NULL) {
-		this->pWatch = NULL;
-	}
-	return p;
-}
-
-
-bool Smalltalk::giveWatch(Watch * pWatch) {
-
-	if (this->pWatch != NULL) {
-		return false;
-	} else {
-		this->pWatch = pWatch;
-		return true;
-	}
-}
