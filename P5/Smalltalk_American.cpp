1086de824e644070093b89d8937e3909c3593b89
Brady Pittman <34654133+bradypittman@users.noreply.github.com>
2018-03-28 21:51:44 -0400

    Update Smalltalk_American.cpp

--- P5/Smalltalk_American.cpp
+++ P5/Smalltalk_American.cpp
@@ -1,17 +1,32 @@
-#pragma once
-#include <vector>
-#include "Smalltalk.h"
+/*
+ * Smalltalk_American.cpp
+ *
+ *  Created on: Nov 8, 2017
+ *      Author: Brady
+ */
 
-class Smalltalk_American :
-	public Smalltalk
-{
-public:
-	//create with appropriate phrases
-	//use base class call to set Nationality (See constants for Nationality strings)
-	Smalltalk_American(int i =1);
-	Smalltalk_American(std::string myNationality,int i =1);
-	
-	~Smalltalk_American(void);
-	virtual void populatePhrases();
-};
 
+#include "./includes/Smalltalk_American.h"
+#include "./includes/constants.h"
+
+Smalltalk_American::Smalltalk_American(int iPerson)
+	: Smalltalk(AMERICAN, iPerson) {
+
+	populatePhrases();
+}
+
+Smalltalk_American::Smalltalk_American(std::string myNationality, int iPerson)
+	: Smalltalk(myNationality, iPerson) {
+}
+
+Smalltalk_American::~Smalltalk_American(void) {
+}
+
+void Smalltalk_American::populatePhrases() {
+
+	mySmallTalk.push_back(AMERICAN_PHRASE_1);
+	mySmallTalk.push_back(AMERICAN_PHRASE_2);
+	mySmallTalk.push_back(AMERICAN_PHRASE_3);
+	mySmallTalk.push_back(AMERICAN_PHRASE_4);
+	mySmallTalk.push_back(AMERICAN_PHRASE_5);
+}
